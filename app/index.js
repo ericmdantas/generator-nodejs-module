27788cd488ea51e40c581ca8f7155bbe3a816ff7
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-03-22 11:37:47 -0300

    using babel's es6 transpiler, updated README

--- app/index.js
+++ app/index.js
@@ -1,80 +1,77 @@
 "use strict";
 
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var util = require('util');
+var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
 
-var NodeJsModule = function(args, options, config)
-{
-  yeoman.generators.Base.apply(this, arguments);
-}
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
-util.inherits(NodeJsModule, yeoman.generators.Base);
+var yeoman = require("yeoman-generator");
+var chalk = require("chalk");
+var yosay = require("yosay");
 
-NodeJsModule.prototype.initializing = function()
-{
-  this.pkg = require('../package.json');
-}
+var NodeJsModule = (function (_yeoman$generators$Base) {
+  function NodeJsModule(args, options, config) {
+    _classCallCheck(this, NodeJsModule);
 
-NodeJsModule.prototype.prompting = function()
-{
-  this.log(yosay('Welcome to the amazing' + chalk.green(' NodeJS Module ') + ' generator!'));
-}
+    yeoman.generators.Base.apply(this, arguments);
+  }
 
-NodeJsModule.prototype.writing = function()
-{
-  var _app = {app: this.appName};
-  var _usernameGithub = {username: this.githubUsername};
-  var _appNameAndUsernameGithub = {username: _usernameGithub.username, app: _app.app};
+  _inherits(NodeJsModule, _yeoman$generators$Base);
 
-  this.fs.copy(this.templatePath('_index.js'), this.destinationPath('index.js'));
-  this.fs.copy(this.templatePath('lib/_libIndex.js'), this.destinationPath('lib/index.js'));
-  this.fs.copy(this.templatePath('tests/_app_test.js'), this.destinationPath('tests/app_test.js'));
+  NodeJsModule.prototype.initializing = function initializing() {
+    this.pkg = require("../package.json");
+  };
 
-  this.template('_package.json', 'package.json', _appNameAndUsernameGithub);
-  this.template('_README.md', 'README.md', _appNameAndUsernameGithub);
+  NodeJsModule.prototype.prompting = function prompting() {
+    this.log(yosay("Welcome to the amazing" + chalk.green(" NodeJS Module ") + " generator!"));
+  };
 
-  this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
-  this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
-  this.fs.copy(this.templatePath('.npmignore'), this.destinationPath('.npmignore'));
-  this.fs.copy(this.templatePath('.travis.yml'), this.destinationPath('.travis.yml'));
-  this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
-}
+  NodeJsModule.prototype.writing = function writing() {
+    var _app = { app: this.appName };
+    var _usernameGithub = { username: this.githubUsername };
+    var _appNameAndUsernameGithub = { username: _usernameGithub.username, app: _app.app };
 
-NodeJsModule.prototype.install = function()
-{
-  var _installOpts = {skipInstall: this.options['skip-install'], bower: false, npm: true};
+    this.fs.copy(this.templatePath("_index.js"), this.destinationPath("index.js"));
+    this.fs.copy(this.templatePath("lib/_libIndex.js"), this.destinationPath("lib/index.js"));
+    this.fs.copy(this.templatePath("tests/_app_test.js"), this.destinationPath("tests/app_test.js"));
 
-  this.installDependencies(_installOpts);
-}
+    this.template("_package.json", "package.json", _appNameAndUsernameGithub);
+    this.template("_README.md", "README.md", _appNameAndUsernameGithub);
 
-NodeJsModule.prototype.showPrompts = function()
-{
-  var done = this.async();
+    this.fs.copy(this.templatePath("jshintrc"), this.destinationPath(".jshintrc"));
+    this.fs.copy(this.templatePath(".gitignore"), this.destinationPath(".gitignore"));
+    this.fs.copy(this.templatePath(".npmignore"), this.destinationPath(".npmignore"));
+    this.fs.copy(this.templatePath(".travis.yml"), this.destinationPath(".travis.yml"));
+    this.fs.copy(this.templatePath("editorconfig"), this.destinationPath(".editorconfig"));
+  };
 
-  var prompts =
-  [
-    {
-      name: 'appName',
-      message: 'What is the name of your module?',
-      default: 'an-awesome-name-goes-here'
-    },
-    {
-      name: 'githubUsername',
-      message: 'What is your username on Github?',
-      default: 'an-awesome-username-goes-here'
-    }
-  ];
+  NodeJsModule.prototype.install = function install() {
+    var _installOpts = { skipInstall: this.options["skip-install"], bower: false, npm: true };
 
-  this.prompt(prompts, function(props)
-  {
-    this.appName = props.appName;
-    this.githubUsername = props.githubUsername;
+    this.installDependencies(_installOpts);
+  };
 
-    done();
+  NodeJsModule.prototype.showPrompts = function showPrompts() {
+    var done = this.async();
 
-  }.bind(this));
-}
+    var prompts = [{
+      name: "appName",
+      message: "What is the name of your module?",
+      "default": "an-awesome-name-goes-here"
+    }, {
+      name: "githubUsername",
+      message: "What is your username on Github?",
+      "default": "an-awesome-username-goes-here"
+    }];
+
+    this.prompt(prompts, (function (props) {
+      this.appName = props.appName;
+      this.githubUsername = props.githubUsername;
+
+      done();
+    }).bind(this));
+  };
+
+  return NodeJsModule;
+})(yeoman.generators.Base);
 
 module.exports = NodeJsModule;
